9c287d1c1448d39d0dbdff16eb8c83229d5779d2
dauni6 <45280927+dauni6@users.noreply.github.com>
2023-10-08 14:07:44 +0900

    feat : SplashScreen 보여주기
    color 리소스에 red,blue컬러를 새롭게 만들어주고 theme.xml에 Splash screen에 보여줄 스타일을 새로 만듦.

--- app/src/main/java/dev/dontsu/coinmonitering/ui/IntroActivity.kt
+++ app/src/main/java/dev/dontsu/coinmonitering/ui/IntroActivity.kt
@@ -2,11 +2,15 @@ package dev.dontsu.coinmonitering.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dev.dontsu.coinmonitering.R
 
 class IntroActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
 
